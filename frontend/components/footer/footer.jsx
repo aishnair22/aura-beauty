b3a2ba1cad7237fcaa72bf64ea8725dd2da2b896
aishnair22 <aishwarya.nair22@gmail.com>
2020-11-10 11:03:34 -0800

    Add meta tags

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -13,8 +13,8 @@ class Footer extends React.Component {
                     <div className="footer-name">
                         by Aishwarya Nair
                     </div>
-                    <a className="footer-link" href="https://github.com/aishnair22/">GITHUB</a>
-                    <a className="footer-link" href="https://www.linkedin.com/in/aishwarya-nair22/">LINKEDIN</a>
+                    <a className="footer-link" href="https://github.com/aishnair22/" target="_blank">GITHUB</a>
+                    <a className="footer-link" href="https://www.linkedin.com/in/aishwarya-nair22/" target="_blank">LINKEDIN</a>
                 </div>
                 <div className="footer-logo">
                     <h1>Aura Beauty</h1>
