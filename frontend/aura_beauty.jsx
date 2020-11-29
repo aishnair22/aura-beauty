555a52665bc1da8f00e6500eeeb13d5e9dd438f6
aishnair22 <aishwarya.nair22@gmail.com>
2020-11-28 16:44:41 -0800

    Edit currentCart on application.html.erb

--- frontend/aura_beauty.jsx
+++ frontend/aura_beauty.jsx
@@ -2,16 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom"
 import configureStore from "./store/store"
 import Root from "./components/root"
-import { deleteCartItem, fetchAllCartItems, createCartItem, updateCartItem } from './actions/cart_item_actions'
+// import { deleteCartItem, fetchAllCartItems, createCartItem, updateCartItem } from './actions/cart_item_actions'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root")
     let preloadedState = undefined;
-    if (window.currentUser) {
+    if (window.currentUser && window.currentCart) {
         preloadedState = {
             session: {
                 currentUser: window.currentUser,
-                currentCart: window.currentUser.cart
+                currentCart: window.currentCart
             }
         };
     }
@@ -19,11 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(<Root store={store} />, root);
 
     //testing
-    window.store = store
-    window.dispatch = store.dispatch
-    window.fetchAllCartItems = fetchAllCartItems
-    window.deleteCartItem = deleteCartItem
-    window.createCartItem = createCartItem
-    window.updateCartItem = updateCartItem
+    // window.store = store
+    // window.dispatch = store.dispatch
+    // window.fetchAllCartItems = fetchAllCartItems
+    // window.deleteCartItem = deleteCartItem
+    // window.createCartItem = createCartItem
+    // window.updateCartItem = updateCartItem
 })
 
